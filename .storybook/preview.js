8c7b6d55cb62546164fb87f175b812dd346551bd
Musango <musangow@gmail.com>
2022-08-12 16:56:08 +0200

    add storybook plus simple story for my-component

---
+++ .storybook/preview.js
@@ -0,0 +1,13 @@
+import {defineCustomElements} from '../dist/packages/core-components/loader';
+
+defineCustomElements();
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
